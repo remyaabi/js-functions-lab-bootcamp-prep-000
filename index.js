2096f12332f11e3e8a402d6ff6fc271630f5c7ce
Remya Jacob <remyaabi@gmail.com>
2018-01-16 01:25:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 // write your code below!
 
 
+<<<<<<< HEAD
 var holiday="Independence Day"
 var days=20
 var name="you"
+=======
+holiday="Independence Day"
+var days=20
+var name="you!"
+>>>>>>> b46aee72ae17cfc57cadaa95e5efde08d4c5ff9c
 
 function happyHolidays(){
   return "Happy holidays!"
 }
 function happyHolidaysTo(name){
+<<<<<<< HEAD
   return `Happy holidays, ${name}!`
 }
 var name="you"
@@ -19,5 +26,19 @@ var holiday="Mother's Day"
 
 function holidayCountdown(holiday, days){
     return `It\'s ${days} days until ${holiday}!`
+=======
+  console.log('Happy holidays, ${name}') 
+  return 'Happy holidays, ${name}'
+}
+function happyHolidayTo(holiday, name){
+ 
+  console.log('Happy ${holiday},${name}') 
+  return 'Happy ${holiday}, ${name}'
+}
+var holiday="Mother's Day"
+function holidayCountdown(holiday, days){
+   console.log("It's ${days} until ${holiday}") 
+    return "It\'s ${days} until ${holiday}"
+>>>>>>> b46aee72ae17cfc57cadaa95e5efde08d4c5ff9c
 }
 
